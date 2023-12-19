b5dd49cc712edf64790ceb3b929cd2cf4690fc66
rajvatsal <rajvatsal2004@gmail.com>
2023-12-19 15:47:44 +0530

    Add fps and game states

--- main.lua
+++ main.lua
@@ -1,9 +1,18 @@
-local love = require('love')
+local love = require("love")
 
 local player = {
   radius = 20,
   x = 30,
   y = 30,
+}
+
+local game = {
+  state = {
+    menu = false,
+    running = true,
+    pause = false,
+    ended = false,
+  }
 }
 
 love.load = function()
@@ -15,5 +24,14 @@ love.update = function()
 end
 
 love.draw = function()
-  love.graphics.circle('fill', player.x, player.y, player.radius)
+  love.graphics.printf(love.timer.getFPS(), love.graphics.newFont(14), 10,
+    love.graphics.getHeight() - 30, love.graphics.getWidth())
+
+  if game.state["running"] then
+    love.graphics.circle("fill", player.x, player.y, player.radius)
+  end
+
+  if not game.state["running"] then
+    love.graphics.circle("fill", player.x, player.y, player.radius / 2)
+  end
 end
